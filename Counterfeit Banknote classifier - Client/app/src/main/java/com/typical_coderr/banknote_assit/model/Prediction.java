f108312ee38daa32712038e6c9ccd47882245009
lahiru de silva <desilvalahiru4@gmail.com>
2022-02-12 19:52:50 +0530

    android app

---
+++ Counterfeit Banknote classifier - Client/app/src/main/java/com/typical_coderr/banknote_assit/model/Prediction.java
@@ -0,0 +1,16 @@
+package com.typical_coderr.banknote_assit.model;
+
+import java.util.List;
+
+/**
+ * Created by Android Studio.
+ * User: Lahiru
+ * Date: Tue
+ * Time: 11:09 PM
+ */
+public class Prediction {
+
+    public String image_url;
+    public String response;
+
+}
